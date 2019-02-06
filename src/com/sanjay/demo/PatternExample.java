2da0d114c5118fd98a2b5695486b2441e0a710ec
sanjay <sanjaykrao1290@gmail.com>
2019-02-06 14:10:53 -0600

    unstage to stage index

--- src/com/sanjay/demo/PatternExample.java
+++ src/com/sanjay/demo/PatternExample.java
@@ -7,6 +7,7 @@ public class PatternExample {
 				System.out.print("*");
 			}
 			System.out.println();
+			
 		}
 	}
 
---
+++ src/com/sanjay/demo/Sum2.java
@@ -0,0 +1,23 @@
+package com.sanjay.demo;
+
+public class Sum2 {
+
+	public static void main(String[] args) {
+		int a=5,b=8;
+		int sum=0;
+		sum = a+b;
+		System.out.println("sum of a and b is="+sum);
+		Integer c=8;
+		Integer d=9;
+		Integer sumOfIntegers=c+d;
+		System.out.println("sum of c and d is ="+sumOfIntegers);
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+}
